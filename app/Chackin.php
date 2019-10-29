ff75886ee897202c6b059a0abf2992ce02cbafa3
Ponkasam <51359218+Ponkasam@users.noreply.github.com>
2019-10-29 19:44:36 +0700

    fix and add controller

--- app/Chackin.php
+++ app/Chackin.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Chackin extends Model
 {
-    //
+    protected $table = 'chackins';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'stud_id','subject_name','status'
+    ];
 }
